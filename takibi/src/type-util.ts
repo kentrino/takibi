279b771a24a1715df5c17a1382f4bb53dffac674
kentrino <kentrino@h-kento.jp>
2026-08-31 23:58:20 +0900

    refactor(takibi): centralize shared type utilities

--- takibi/src/type-util.ts
+++ takibi/src/type-util.ts
@@ -1,4 +1,4 @@
-type IsAny<T> = 0 extends 1 & T ? true : false;
+export type IsAny<T> = 0 extends 1 & T ? true : false;
 
 /**
  * Extracts keys whose value types are assignable to `TType`.
@@ -15,3 +15,23 @@ export type KeysMatching<TObject, TType> = {
         ? TKey
         : never;
 }[keyof TObject];
+
+export type StringKeysMatching<TObject, TType> = Extract<KeysMatching<TObject, TType>, string>;
+
+export type HasDuplicateTupleMember<TTuple extends readonly unknown[]> = TTuple extends readonly [
+  infer Head,
+  ...infer Tail,
+]
+  ? Head extends Tail[number]
+    ? true
+    : HasDuplicateTupleMember<Tail>
+  : false;
+
+export type DistributiveOmit<TObject, TKeys extends PropertyKey> = TObject extends unknown
+  ? Omit<TObject, TKeys>
+  : never;
+
+export type ForbidKeys<TObject, TKeys extends PropertyKey> =
+  Extract<keyof TObject, TKeys> extends never
+    ? unknown
+    : { [TKey in Extract<keyof TObject, TKeys>]?: never };
