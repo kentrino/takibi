71addf5e496ac14e488b445c64b4be70d232568d
kentrino <kentrino@h-kento.jp>
2026-09-18 20:37:10 +0900

    refactor(issue-tracker): use ~/ for internal src imports

--- examples/issue-tracker/src/index.ts
+++ examples/issue-tracker/src/index.ts
@@ -1,8 +1,8 @@
-export { createIssueTrackerHandler, FULL_PATH_FIXTURE } from "./handler.ts";
+export { createIssueTrackerHandler, FULL_PATH_FIXTURE } from "~/handler.ts";
 export {
   FULL_PATH_EXPECTED_CHECKSUM,
   FULL_PATH_OPERATIONS,
   fullPathBenchmarkSink,
   runFullPathScenario,
   type FullPathScenarioReport,
-} from "./full-path-scenario.ts";
+} from "~/full-path-scenario.ts";
